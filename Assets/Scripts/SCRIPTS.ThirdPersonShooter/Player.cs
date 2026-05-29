ef9c48e7ab1acb4fcc3a0082afa803e55014d43a
ducheanidi-designs <ducheanidi@gmail.com>
2026-05-29 06:53:50 +0100

    Game2 Build...

--- Assets/Scripts/SCRIPTS.ThirdPersonShooter/Player.cs
+++ Assets/Scripts/SCRIPTS.ThirdPersonShooter/Player.cs
@@ -8,10 +8,18 @@ public class Player : MonoBehaviour
 
     [SerializeField] private PlayerInput playerInput;
     [SerializeField] private InputAction moveAction;
+    [SerializeField] private InputAction lookAction;
+
+    [SerializeField] private Transform canTarget;
+    [SerializeField] private Vector2 pitchClampValue;
+
+    [SerializeField] private Animator anim;
+    private float xRotation = 0f;
 
     void Start()
     {
         moveAction = playerInput.actions["Move"];
+        lookAction = playerInput.actions["Look"];
     }
 
     void Update()
@@ -20,9 +28,32 @@ public class Player : MonoBehaviour
 
         inputVector = inputVector.normalized;
 
-        Vector3 movDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        Vector3 movDir = transform.right * inputVector.x + transform.forward * inputVector.y;
         transform.position += movDir * moveSpeed* Time.deltaTime;
-        
-        transform.forward = Vector3.Slerp(transform.forward, movDir, rotateSpeed * Time.deltaTime);
+
+        // transform.forward = Vector3.Slerp(transform.forward, movDir, rotateSpeed * Time.deltaTime);
+
+       Vector2 lookInput = lookAction.ReadValue<Vector2>();
+       Debug.Log(lookInput);
+
+       HandleRotation();
+
+    }
+
+    void HandleRotation()
+    {
+        Vector2 lookInput = lookAction.ReadValue<Vector2>();
+
+        // LEFT/RIGHT - Rotate camera target (and player follows)
+        transform.Rotate(Vector3.up * lookInput.x);
+
+        // player faces camera direction
+        float camYaw = transform.eulerAngles. y;
+        transform.rotation = Quaternion.Euler(0, camYaw, 0);
+
+        //UP/DOWN
+        xRotation -= lookInput.y;
+        xRotation = Mathf. Clamp(xRotation, -pitchClampValue.x, pitchClampValue.y);
+        canTarget.rotation = Quaternion.Euler(xRotation, camYaw, 0f);
     }
 }
